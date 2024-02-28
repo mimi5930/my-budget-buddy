a24cc651cf7582d83ae7c63b7c097394d3a4f0c9
Mike Miller <mikej.miller440@gmail.com>
2024-02-27 21:32:00 -0600

    added file submission route

--- src/routes/api/transaction/+server.ts
+++ src/routes/api/transaction/+server.ts
@@ -1,20 +1,25 @@
-import { error, json, type RequestHandler } from '@sveltejs/kit';
-import { read } from '$app/server';
+import { json, type RequestHandler } from '@sveltejs/kit';
+import crypto from 'crypto';
 import csv from 'csvtojson';
-import { parse } from 'csv-parse';
-import * as fs from 'fs';
-import * as data from '$lib/data/data.json';
 
 type RawData = { Date: string; Amount: string; Balance: string; Description: string }[];
 
 export const POST: RequestHandler = async ({ request }) => {
-	// TODO: remove hard coded json
+	// create json from csv file input
+	const formData = Object.fromEntries(await request.formData());
+	const { file } = formData as { file: File };
+	const fileData = await file.text();
+	const jsonData = await csv().fromString(fileData);
 
+	// sanitize data
 	const cleanData = (rawData: RawData) => {
 		const newData = [];
 		for (const obj of rawData) {
 			const { Date, Description, Amount, Balance } = obj;
+			// create a hash to ensure no records are duplicated in db
+			const uniqueHashString = Date + Description + Balance;
 			const newObj = {
+				id: crypto.createHash('sha1').update(uniqueHashString).digest('hex'),
 				date: Date,
 				description: Description.replace(/  +/g, ' '),
 				amount: parseFloat(Amount.replace(',', '').replace('$', '')),
@@ -26,5 +31,5 @@ export const POST: RequestHandler = async ({ request }) => {
 		return newData;
 	};
 
-	return json(cleanData(data.default));
+	return json(cleanData(jsonData));
 };
